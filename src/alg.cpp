3b64e77f67dda2b983c2f7d6dc2d824b0ab99f46
Alexey Kulikov <125137195+en8eez@users.noreply.github.com>
2024-03-24 15:04:59 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,38 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int found = 0;
+  int left = 0;
+  int right = size - 1;
+  int middle = 0;
+  while ((left <= right)) {
+    middle = (left + right) / 2;
+    if (arr[middle] == value) {
+      int tmp = middle-1;
+      while (arr[middle] == value) {
+        if (arr[middle] == value) {
+          middle++;
+          found++;
+        } else {
+          break;
+        }
+      }
+      while (arr[tmp] == value) {
+        if (arr[tmp] == value) {
+          --tmp;
+          found++;
+        } else {
+          break;
+        }
+      }
+      break;
+    }
+    if (arr[middle] > value) {
+      right = middle - 1;
+    } else {
+      left = middle + 1;
+    }
+  }
+    return found;
+  return 0;
 }
